de0d7d03551d2addad9ecb3cc44e794b03cdbacd
Gustavo Torres <gtvtorres@gmail.com>
2018-12-18 14:49:21 -0200

    ajustes de JS

--- js/js_application.js
+++ js/js_application.js
@@ -6,27 +6,27 @@ app.config(function($routeProvider) {
         controller: "defaultController"
     })
     .when("/minha_rota", {
-        templateUrl : "/templates/minha_rota.html",
+        templateUrl : "./templates/minha_rota.html",
         controller:   "minhaRotaController"
     })
     .when("/meus_clientes", {
-        templateUrl : "/templates/meus_clientes.html",
+        templateUrl : "./templates/meus_clientes.html",
         controller:   "meusClientesController"
     })
     .when("/catalogo_produtos", {
-        templateUrl : "/templates/catalogo_produtos.html",
+        templateUrl : "./templates/catalogo_produtos.html",
         controller:   "catalogoProdutosController"
     })
     .when("/estoque_precos", {
-        templateUrl : "/templates/estoque_precos.html",
+        templateUrl : "./templates/estoque_precos.html",
         controller:   "estoquePrecosController"
     })
     .when("/relatorios", {
-        templateUrl : "/templates/relatorios.html",
+        templateUrl : "./templates/relatorios.html",
         controller:   "relatoriosController"
     })
     .when("/agendar_visita", {
-        templateUrl : "/templates/agendar_visita.html",
+        templateUrl : "./templates/agendar_visita.html",
         controller:   "agendarVisitaController"
     })
     .otherwise({redirectTo:"/home"});
