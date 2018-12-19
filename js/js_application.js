e6d4a5f6deb3a648700fee629e4793312776d998
Gustavo Torres <gtvtorres@gmail.com>
2018-12-19 09:45:24 -0200

    ajustes interface

--- js/js_application.js
+++ js/js_application.js
@@ -35,3 +35,17 @@ app.config(function($routeProvider) {
     })
     .otherwise({redirectTo:"/home"});
 });
+
+angular.module('tabApp', [])
+  .controller('TabController', ['$scope', function($scope) {
+    $scope.tab = 1;
+
+    $scope.setTab = function(newTab){
+      $scope.tab = newTab;
+    };
+
+    $scope.isSet = function(tabNum){
+      return $scope.tab === tabNum;
+    };
+}]);
+
