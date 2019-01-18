24605b2197fb0bc93252492ed7edc93c980a99f7
Gustavo Torres <gtvtorres@gmail.com>
2019-01-18 17:24:45 -0200

    Layout design HTML, CSS

--- js/jQuery-custom.js
+++ js/jQuery-custom.js
@@ -47,3 +47,18 @@ $(document).ready(function () {
 function goBack() {
     window.history.back();
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
