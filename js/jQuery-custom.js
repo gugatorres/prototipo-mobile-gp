366ea51071323763589e8d019d8072472c1c419c
Gustavo Torres <gtvtorres@gmail.com>
2019-01-21 14:22:09 -0200

    Layout design HTML, CSS

--- js/jQuery-custom.js
+++ js/jQuery-custom.js
@@ -68,7 +68,7 @@ function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
+    ul = document.getElementById("tipos-pagamento");
     li = ul.getElementsByTagName('li');
   
     // Loop through all list items, and hide those who don't match the search query
