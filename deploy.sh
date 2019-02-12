78fe12d49d531ff9a62d53964c84840a53f12221
Daniel Klava <klava.daniel@gmail.com>
2019-02-12 15:00:48 -0200

    Fix script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 
 #deploy fazendo upload via FTP
 lftp -c "set net:timeout 5; set ftp:ssl-allow no;
-open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21; mirror -Rv . .; quit;"
+open ftp://$FTP_USER:$FTP_PASSWORD@$FTP_HOST:21; mirror -Rv . .; quit;"
