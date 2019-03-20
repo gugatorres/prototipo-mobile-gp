38ff86f97d3cc44a5dd271d827b0334557572707
Gustavo Torres <gtvtorres@gmail.com>
2019-03-20 11:44:09 -0300

    ajustes de Interface - HTML, CSS

--- js/js_controller.js
+++ js/js_controller.js
@@ -871,13 +871,7 @@ app.controller("defaultController", function ($scope) {
 			window.location.href = "#!/comodatos_pendentes";
 		});
 
-		angular.element(".btn-sim").click(function () {
-			angular.element(".checkin-cliente").css("display", "none");
-		});
-
-		angular.element(".btn-nao").click(function () {
-			angular.element(".checkin-cliente").css("display", "none");
-		});
+		
 
 		$scope.contratocomodatos = function () {
 			window.location.href = "#!/comodatos_contrato";
