4bd792a56c3447edcb765333427b354e85b9de3c
Gustavo Torres <gtvtorres@gmail.com>
2019-04-12 14:28:43 -0300

    ajustes de Interface - HTML, CSS

--- js/js_controller.js
+++ js/js_controller.js
@@ -926,7 +926,7 @@ app.controller("defaultController", function ($scope) {
 
 
 		angular.element(".add-product").click(function () {
-			angular.element(".overlay-modal").not('.cart-clean').css("display", "block");
+			angular.element(".overlay-product").not('.cart-clean').css("display", "block");
 		});
 
 		angular.element(".close-modal").click(function () {
